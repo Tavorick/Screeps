68f0146be491ce59c26007fff11f55330967eb3f
Lars <lars.picke@gmail.com>
2016-12-29 23:12:41 +0100

    Code improvement

--- src/main.js
+++ src/main.js
@@ -5,29 +5,19 @@ let populater = require('./populater');
 let autodeployerstructure = require('./autodeployer.structure');
 let rolerepairer = require('./role.repairer');
 let roleSentryDrone = require('./role.sentrydrone');
+let roletower = require('./role.tower;')
 module.exports.loop = function ()
 {
 
     let towers = Game.rooms.E61S78.find(FIND_STRUCTURES, {
         filter: (s) => s.structureType == STRUCTURE_TOWER
     });
-    let tower = towers[0];
-    if (tower == STRUCTURE_TOWER)
+    for (towerid in towers)
     {
-        let closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if (closestDamagedStructure)
-        {
-            tower.repair(closestDamagedStructure);
-        }
-
-        let closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if (closestHostile)
-        {
-            tower.attack(closestHostile);
-        }
+        let tower = towers[towerid];
+        roletower.run(tower);
     }
+
     autodeployerstructure.run();
     populater.run();
     for (let name in Game.creeps)
