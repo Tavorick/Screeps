329c1c8c21dbaf0fea5587b1b09936381069bce9
Lars <lars.picke@gmail.com>
2017-01-01 15:20:51 +0100

    Code improvement and bug fixes

--- src/role.tower.js
+++ src/role.tower.js
@@ -3,8 +3,9 @@ let tower =
         run:function(tower)
         {
             let closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: (structure) => structure.hits < structure.hitsMax
+                filter: (structure) => structure.hits < structure.hitsMax && structure != STRUCTURE_WALL
             });
+
             if (closestDamagedStructure)
             {
                 tower.repair(closestDamagedStructure);
