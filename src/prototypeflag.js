154dc589564114690982f8171eaadd397b5eada3
Lars <lars.picke@gmail.com>
2017-01-04 19:21:51 +0100

    Code cleanup

--- src/prototypeflag.js
+++ src/prototypeflag.js
@@ -1,9 +1,9 @@
 Flag.prototype.run = function()
 {
-    let room = Game.rooms[this.room];
+
     if (this.memory.mineroom == true)
     {
-        this.PrepareMining(room);
+        this.PrepareMining();
     }
 
     if (this.memory.claimroom == true)
@@ -15,29 +15,5 @@ Flag.prototype.run = function()
 
 Flag.prototype.PrepareMining = function(room)
 {
-    let sources = room.find(FIND_SOURCES);
-    // iterate over all sources
-    for (let source of sources)
-    {
-        let containers = source.pos.findInRange(FIND_STRUCTURES, 1,
-            {
-                filter: s => s.structureType == STRUCTURE_CONTAINER
-            });
-
-        if (containers.length > 0)
-        {
-          outerloop:
-            for (i = room.pos.x - 1; i < room.pos.x + 2 ; i++)
-            {
-                for (t = room.pos.y - 1; t < room.pos.y + 2; t++ )
-                {
-                    if (room.createConstructionSite(i,t,STRUCTURE_CONTAINER) == 0)
-                    {
-                        break outerloop;
-                    }
-                }
-            }
-        }
-        this.memory.mineroom= false;
-    }
+    Game.memory.remoteminingtargets.push(this.pos);
 };
